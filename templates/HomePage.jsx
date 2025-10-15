3362ed4fffae2d5c3dcb00c672bd0f88014f40a7
Salar Safayi <108089493+zalar2202@users.noreply.github.com>
2025-10-15 17:36:54 +0300

    update-homepage

--- templates/HomePage.jsx
+++ templates/HomePage.jsx
@@ -15,7 +15,7 @@ export default function HomePage() {
                                 component="h1"
                                 gutterBottom
                             >
-                                گروه مهاجرتی امیدار
+                                گروه مهاجرتی Vista
                             </Typography>
                             <Typography
                                 variant="h4"
